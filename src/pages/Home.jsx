c0198f5aebeccc8d52a1619695ea571f45269951
Mark Lorens <marklorens0705@gmail.com>
2026-03-16 11:37:21 +0800

    Fix import casing for Hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Footer from "../components/Footer";
 import ClientReview from "../components/home/ClientReview";
 import CTA from "../components/home/CTA";
 import FAQ from "../components/home/FAQ";
-import Hero from "../components/home/hero";
+import Hero from "../components/home/Hero";
 import Services from "../components/home/Services";
 import VideoShowcase from "../components/home/VideoShowcase";
 import WorkFlow from "../components/home/WorkFlow";
