11b14ad4a19c965edba186f59476f9cb08553e5f
Mark Lorens <marklorens0705@gmail.com>
2026-03-16 11:49:05 +0800

    Fixed base config path.

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.HEIC"],
-  base: "/SettleSmart/",
+  base: "/",
 });
