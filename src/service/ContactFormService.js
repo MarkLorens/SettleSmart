88f9bfd9c925d3ab88e8462104c009ec39c81907
Mark Lorens <marklorens0705@gmail.com>
2026-05-04 12:46:03 +0800

    Added calledBack bool to firebase.

--- src/service/ContactFormService.js
+++ src/service/ContactFormService.js
@@ -4,6 +4,7 @@ import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 export const createContactFrom = async (contactForm) => {
   return addDoc(collection(db, "ContactForms"), {
     ...contactForm,
+    calledBack: false,
     createdAt: serverTimestamp(),
   });
 };
